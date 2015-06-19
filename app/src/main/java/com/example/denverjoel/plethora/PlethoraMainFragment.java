694e99ab858b5ba460330fb795fbe0eaa13f42f1
denverjoel <twistyourthumbs@gmail.com>
2015-06-18 22:31:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/denverjoel/plethora/PlethoraMainFragment.java
@@ -0,0 +1,23 @@
+package com.example.denverjoel.plethora;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlethoraMainFragment extends Fragment {
+
+    public PlethoraMainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_plethora_main, container, false);
+    }
+}
